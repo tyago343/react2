fa70cee3e8e783ae672311eddc137d6cc6bedc70
Guillermo Aszyn <guilleaszyn@gmail.com>
2018-01-15 16:57:26 -0300

    currentSongList

--- browser/react/containers/Main.jsx
+++ browser/react/containers/Main.jsx
@@ -14,7 +14,7 @@ export default class Main extends React.Component {
       selectedAlbum: {},
       selectedSong: {},
       isPlaying: false,
-      playlist: [],
+      currentSongList: [],
       progress: 0,
     };
     this.selectAlbum = this.selectAlbum.bind(this);
@@ -51,7 +51,7 @@ export default class Main extends React.Component {
   }
 
   start(song, songs) {
-    this.setState({ selectedSong: song, playlist: songs })
+    this.setState({ selectedSong: song, currentSongList: songs })
     this.loadSong(song.audioUrl);
   }
 
@@ -72,15 +72,15 @@ export default class Main extends React.Component {
   }
   
   findSongIndex() {
-    return this.state.playlist.findIndex(song => song.id === this.state.selectedSong.id);
+    return this.state.currentSongList.findIndex(song => song.id === this.state.selectedSong.id);
   }
 
   next() {
     let index = this.findSongIndex() + 1;
-    if (index >= this.state.playlist.length) {
+    if (index >= this.state.currentSongList.length) {
       index = 0 
     }
-    const song = this.state.playlist[index];
+    const song = this.state.currentSongList[index];
     this.setState({ selectedSong: song })
     this.loadSong(song.audioUrl)
   }
@@ -88,9 +88,9 @@ export default class Main extends React.Component {
   previous() {
     let index = this.findSongIndex() - 1;
     if (index < 0) {
-      index = this.state.playlist.length - 1 
+      index = this.state.currentSongList.length - 1 
     }
-    const song = this.state.playlist[index];
+    const song = this.state.currentSongLists[index];
     this.setState({ selectedSong: song })
     this.loadSong(song.audioUrl)
   }
